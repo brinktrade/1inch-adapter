78d74e90d9664f8eb77da947e9f16f201a068872
Kyle <the.big.kb123@gmail.com>
2021-12-09 12:02:32 -0500

    Updating oneInchAdapter

--- contracts/adapter/OneInchAdapter.sol
+++ contracts/adapter/OneInchAdapter.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.7.6;
 pragma abicoder v2;
 
-import '../1inch/AggregationRouterV4.sol';
 import '../1inch/math/SafeMath.sol';
 import '../1inch/token/SafeERC20.sol';
 import '../1inch/helpers/UniERC20.sol';
@@ -12,11 +11,18 @@ contract OneInchAdapter is Withdrawable {
   using SafeMath for uint256;
   using UniERC20 for IERC20;
   using SafeERC20 for IERC20;
+
   address constant AGG_ROUTER_V4 = 0x1111111254fb6c44bAC0beD2854e76F90643097d;
+  uint256 MAX_INT = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
   constructor (address _owner) Withdrawable(_owner) {}
 
-  function tokenToToken(bytes memory data, IERC20 tokenOut, uint tokenOutAmount, address payable account) external {
+  function oneInchSwap(bytes memory data, IERC20 tokenIn, uint tokenInAmount, IERC20 tokenOut, uint tokenOutAmount, address payable account) external {
+    if (!tokenIn.isEth()) {
+      if (tokenIn.allowance(address(this), AGG_ROUTER_V4) < tokenInAmount) {
+        tokenIn.approve(AGG_ROUTER_V4, MAX_INT);
+      }
+    }
     assembly {
       let result := call(gas(), AGG_ROUTER_V4, callvalue(), add(data, 0x20), mload(data), 0, 0)
       returndatacopy(0, 0, returndatasize())
