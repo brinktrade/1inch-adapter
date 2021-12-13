ac6b65c1ef38ef8a86f880a921f24b94ae30c180
Kyle <the.big.kb123@gmail.com>
2021-12-13 13:42:07 -0500

    Updating to GPL 3 and solc 0.8.10

--- contracts/token/IERC20.sol
+++ contracts/token/IERC20.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity =0.8.10;
 
 /**
  * @dev Interface of the ERC20 standard as defined in the EIP.
